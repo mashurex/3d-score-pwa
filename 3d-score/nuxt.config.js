b10228584e4863e3d79d91e57bc596ada8b9b75a
Mustafa Ashurex <mustafa@cordiance.com>
2021-07-27 17:29:16 -0700

    Enable SSR

--- 3d-score/nuxt.config.js
+++ 3d-score/nuxt.config.js
@@ -1,8 +1,6 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
